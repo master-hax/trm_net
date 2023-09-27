4888f67f0e9aee9a24f5c6fdf86e61de8fbaa2ea
shreelekha <shreelekha.revankar@gmail.com>
2023-09-27 15:51:24 -0400

    maxar downloader

--- trm_net/data/download_maxar.py
+++ trm_net/data/download_maxar.py
@@ -1,5 +1,19 @@
 import leafmap.foliumap as leafmap
 import os
+from os.path import join
+import argparse
+
+#  Example: python download_maxar.py tonga-volcano21 --data_dir data
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='trm-net data downloader for maxar open datasets')
+    parser.add_argument('collection', help='Collection Name ex. tonga-volcano21')
+    parser.add_argument(
+        '--data_dir', help='the dir to save data')
+    parser.add_argument('--all', action='store_true', help='download all collections')
+    args = parser.parse_args()
+    return args
+  
 
 # create function to download Maxar imagery
 def download_maxar(collection_name,data_dir):
@@ -8,9 +22,15 @@ def download_maxar(collection_name,data_dir):
     except:
         print("The collection name is not valid.")
         return
-    if len(collections) == 0:
-        print("No child collections found.")
+    print("downloading " + collection_name + " collection")
+    
+    num_collections = len(collections)
+
+    if num_collections == 0:
+        print("The collection name is not valid.")
         return
+    
+    print("There are " + str(num_collections) + " child collections.")
     
     collections.sort()
 
@@ -21,15 +41,15 @@ def download_maxar(collection_name,data_dir):
             return_gdf=True, 
             assets=['visual'])
         images = gdf['visual'].tolist()
-        download_dir = os.join(data_dir,(collection_name+'/'))
-        print(download_dir)
-        # leafmap.maxar_download(images, out_dir=download_dir)
+        download_dir = join(data_dir,(collection_name))
+        leafmap.maxar_download(images, out_dir=download_dir)
 
+    print("Download completed!")
 
 # main function
 def main():
     args = parse_args()
-    if args.collection_name is None:
+    if args.collection is None:
         print("Please specify the collection name.")
         return
     if args.data_dir is None:
@@ -40,7 +60,7 @@ def main():
         for collection in collections:
             download_maxar(collection,args.data_dir)
     else:
-        download_maxar(args.collection_name,args.data_dir)
+        download_maxar(args.collection,args.data_dir)
     
 if __name__ == "__main__":
     main()
